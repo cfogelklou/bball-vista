ff8cabd341408c6537881281e0252905a4032410
Chris Fogelklou <chris.fogelklou@polestar.com>
2020-09-09 22:42:57 +0200

    Dont proceed until minutes are set.

--- src/bball_logic.ts
+++ src/bball_logic.ts
@@ -1,5 +1,4 @@
 import AsyncStorage from '@react-native-community/async-storage';
-import deepEqual from 'deep-equal';
 
 let singleton: null | BballLogic = null;
 
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Bball } from '../screens/bball';
+import { Settings } from '../screens/settings';
 
 const Routes = (props: any) => {
   return (
     <div>
       <Switch>
-        <Route exact path='/' render={(props) => <Bball {...props} />} />
+        <Route exact path='/' render={(props) => <Settings {...props} />} />
         <Route path='/bball' render={(props) => <Bball {...props} />} />
-        <Route component={Bball} />
+        <Route path='/settings' render={(props) => <Settings {...props} />} />
+        <Route component={Settings} />
       </Switch>
     </div>
   );
